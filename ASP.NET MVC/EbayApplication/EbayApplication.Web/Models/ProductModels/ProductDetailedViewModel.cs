c33adcd33d1b4f99ade2c4cafe9a80fc8ccde06c
Denis Rizov <d.b.rizov@gmail.com>
2013-10-09 11:11:25 +0300

    ASP.NET MVC Teamwork project added

---
+++ ASP.NET MVC/EbayApplication/EbayApplication.Web/Models/ProductModels/ProductDetailedViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace EbayApplication.Web.Models.ProductModels
+{
+    using EbayApplication.Web.Models.AuctionModels;
+    using System.Linq;
+
+    public class ProductDetailedViewModel
+    {
+        public AuctionDetailedViewModel Auction { get; set; }
+
+        public ProductViewModel Product { get; set; }
+
+        public ProductDetailedViewModel()
+        {
+        }
+
+        public ProductDetailedViewModel(ProductViewModel product)
+        {
+            this.Product = product;
+        }
+
+        public ProductDetailedViewModel(ProductViewModel product, AuctionDetailedViewModel auction)
+            :this(product)
+        {
+            this.Auction = auction;
+        }
+
+        public IQueryable<ProductFlatViewModel> SimilarProducts { get; set; }
+    }
+}
